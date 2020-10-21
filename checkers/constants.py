83e1e4cba0ddd621e1abe1c9848458c3ab98cc83
Jonathan Herring <jonathan.d.herring@gmail.com>
2020-10-21 13:43:47 -0700

    second commit

---
+++ checkers/constants.py
@@ -0,0 +1,12 @@
+import pygame
+
+#setting up the board's parameters
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+# rgb color values
+RED = (255, 0, 0)
+WHITE = ( 255 , 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
