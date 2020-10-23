e7794efa7fe39dfe7c29c1a2fe099487b7244e26
Jonathan Herring <jonathan.d.herring@gmail.com>
2020-10-23 14:20:07 -0700

    fixed position bug

--- checkers/piece.py
+++ checkers/piece.py
@@ -1,7 +1,8 @@
-from .constants import RED, WHITE, SQUARE_SIZE, GREY
+from .constants import RED, WHITE, SQUARE_SIZE, GREY, CROWN
+import pygame
 
 class Piece:
-	PADDING = 10
+	PADDING = 15
 	OUTLINE = 2
 
 	def __init__(self, row, col, color):
@@ -17,6 +18,7 @@ class Piece:
 		
 		self.x = 0
 		self.y = 0
+		self.calc_pos()
 	
 	def calc_pos(self):
 		self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
@@ -29,6 +31,8 @@ class Piece:
 		radius = SQUARE_SIZE//2 - self.PADDING
 		pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
 		pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+		if self.king:
+			win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height()//2))
 
 		def __repr__(self):
 			return str(self.color)
